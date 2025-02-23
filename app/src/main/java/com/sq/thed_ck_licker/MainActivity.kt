13c1fda34212746264de0cfb053f194306e9642f
unknown <uurtimo@gmail.com>
2025-02-23 22:38:24 +0200

    mvp hp bar

--- app/src/main/java/com/sq/thed_ck_licker/MainActivity.kt
+++ app/src/main/java/com/sq/thed_ck_licker/MainActivity.kt
@@ -7,10 +7,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.sq.thed_ck_licker.player.HealthBar
 import com.sq.thed_ck_licker.ui.theme.TheD_ck_LickerTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +20,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             TheD_ck_LickerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    HealthBar(0f, modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TheD_ck_LickerTheme {
-        Greeting("Android")
+        HealthBar(0f)
     }
 }
---
+++ app/src/main/java/com/sq/thed_ck_licker/player/Player.kt
@@ -0,0 +1,29 @@
+package com.sq.thed_ck_licker.player
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HealthBar(health: Float, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.fillMaxWidth()) {
+        Text(
+            text = "Player Health",
+            modifier = modifier
+        )
+        LinearProgressIndicator(
+            progress = { health },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(15.dp),
+            color = Color.LightGray,
+            trackColor = Color.Red, //remaining health
+        )
+    }
+}
