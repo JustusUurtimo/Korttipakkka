2bd879a6eb38d8c1689fde342bcd5df5de1e9c61
Qmppu842 <6827447+Qmppu842@users.noreply.github.com>
2025-03-23 10:34:33 +0200

    Dunno if this auto optimize imports is that good... One is not supposed to make that kinds of edits. They distract the main goal of the pr.

--- app/src/main/java/com/sq/thed_ck_licker/ecs/components/PlayerComponents.kt
+++ app/src/main/java/com/sq/thed_ck_licker/ecs/components/PlayerComponents.kt
@@ -1,8 +1,5 @@
 package com.sq.thed_ck_licker.ecs.components
 
-import androidx.compose.runtime.MutableIntState
-import androidx.compose.runtime.mutableIntStateOf
-
 // TODO this needs to be mutableStateListOf but i cant get the type to work
 data class DrawDeckComponent(val cards: MutableList<Int> = mutableListOf())
 
