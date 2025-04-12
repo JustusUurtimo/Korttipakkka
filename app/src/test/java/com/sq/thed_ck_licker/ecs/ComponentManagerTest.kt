3f5c0c3dec30b83b174614994ea8bef1c0e8694b
Qmppu842 <6827447+Qmppu842@users.noreply.github.com>
2025-04-12 12:47:16 +0300

    refactor: ComponentManagerTest with TODO

--- app/src/test/java/com/sq/thed_ck_licker/ecs/ComponentManagerTest.kt
+++ app/src/test/java/com/sq/thed_ck_licker/ecs/ComponentManagerTest.kt
@@ -3,9 +3,5 @@ package com.sq.thed_ck_licker.ecs
 import org.junit.Test
 
 class ComponentManagerTest {
-//    @Test
-//    fun `getPlayerID   returns correct ID`() {
-//        // Check if getPlayerID() always returns the predefined PLAYER_ID constant which is 1.
-//        assert(EntityManager.getPlayerID() == 1) { "getPlayerID should return 1" }
-//    }
+        // TODO: press alt+ins and the editor will give you the template
 }
