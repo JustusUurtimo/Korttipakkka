77aa10829cc39233859dae0a3bb56697b1648e86
Qmppu842 <6827447+Qmppu842@users.noreply.github.com>
2025-04-12 12:39:16 +0300

    Added Testing file for component manager.

---
+++ app/src/test/java/com/sq/thed_ck_licker/ecs/ComponentManagerTest.kt
@@ -0,0 +1,11 @@
+package com.sq.thed_ck_licker.ecs
+
+import org.junit.Test
+
+class ComponentManagerTest {
+//    @Test
+//    fun `getPlayerID   returns correct ID`() {
+//        // Check if getPlayerID() always returns the predefined PLAYER_ID constant which is 1.
+//        assert(EntityManager.getPlayerID() == 1) { "getPlayerID should return 1" }
+//    }
+}
