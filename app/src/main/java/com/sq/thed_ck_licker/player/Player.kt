471b17a21c3dd1ada0cbb13a51316ab445cc2c0f
unknown <uurtimo@gmail.com>
2025-04-14 18:54:34 +0300

    refactored the hp bar so that player hp and max hp are drawn inside the bar

--- app/src/main/java/com/sq/thed_ck_licker/player/Player.kt
+++ app/src/main/java/com/sq/thed_ck_licker/player/Player.kt
@@ -1,5 +1,6 @@
 package com.sq.thed_ck_licker.player
 
+import android.graphics.Typeface
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
@@ -10,12 +11,25 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextLayoutResult
+import androidx.compose.ui.text.TextMeasurer
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.drawText
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun HealthBar(
@@ -31,13 +45,18 @@ fun HealthBar(
         targetValue = progress,
         animationSpec = tween(durationMillis = 500)
     )
-    //this is mainly for testing purposes as it helps keep track on the health changes without having to do extra prints.
+    val hpText = "${currentHealth.value.toInt()}/${maxHealth.value.toInt()}"
+    val paint = Paint().asFrameworkPaint().apply {
+        color = Color.White.toArgb()
+        textSize = with(LocalDensity.current) { 16.sp.toPx() }
+    }
 
     Canvas(
         modifier = modifier
             .height(24.dp)
             .fillMaxWidth()
     ) {
+
 
         drawRoundRect(
             color = Color.Gray.copy(alpha = 0.3f),
@@ -56,18 +75,13 @@ fun HealthBar(
                 size = Size(size.width * animatedProgress, size.height)
             )
         }
-
+        drawContext.canvas.nativeCanvas.drawText(
+            hpText,
+            center.x - paint.measureText(hpText) / 2,
+            center.y + paint.textSize / 3,
+            paint
+        )
     }
-    LinearProgressIndicator(
-        progress = { (currentHealth.value.toFloat() / 100f) },
-        modifier = modifier
-            .fillMaxWidth()
-            .height(15.dp),
-        color = Color.Red, //remaining health
-        trackColor = Color.LightGray,
-    )
-
-    Text("player Health: ${currentHealth.value.toInt()}", modifier)
 }
 
 @Composable
