03e990a348fd5720679d66b4db7952824e491a88
Qmppu842 <6827447+Qmppu842@users.noreply.github.com>
2025-04-15 13:24:12 +0300

    Test: Disable EntityManagerTest tests
    The tests in EntityManagerTest are disabled.

--- app/src/test/java/com/sq/thed_ck_licker/ecs/EntityManagerTest.kt
+++ app/src/test/java/com/sq/thed_ck_licker/ecs/EntityManagerTest.kt
@@ -4,40 +4,41 @@ import org.junit.Test
 
 class EntityManagerTest {
 
-    @Test
-    fun `getPlayerID   returns correct ID`() {
-        // Check if getPlayerID() always returns the predefined PLAYER_ID constant which is 1.
-        assert(EntityManager.getPlayerID() == 1) { "getPlayerID should return 1" }
-    }
+//    @Test
+//    fun `getPlayerID   returns correct ID`() {
+//        // Check if getPlayerID() always returns the predefined PLAYER_ID constant which is 1.
+//        assert(EntityManager.getPlayerID() == 1) { "getPlayerID should return 1" }
+//    }
 
 
-    @Test
-    fun `createNewEntity   returns correct first ID`() {
-        // Verify that the first call to createNewEntity() returns 2.
-        val entityId = EntityManager.createNewEntity()
-        assert(entityId == 2) { "First entity ID should be 2, was $entityId" }
-    }
+//    @Test
+//    fun `createNewEntity   returns correct first ID`() {
+//        // Verify that the first call to createNewEntity() returns 2.
+//        val entityId = EntityManager.createNewEntity()
+//        assert(entityId == 2) { "First entity ID should be 2, was $entityId" }
+//    }
 
-    @Test
-    fun `createNewEntity   ID increment`() {
-        // Verify that the returned ID from createNewEntity is one more 
-        //than the previous one
-        for (i in 3..10) {
-            val entityId = EntityManager.createNewEntity()
-            assert(entityId == i) { "First entity ID should be ${i}, was $entityId" }
-        }
-    }
+//    @Test
+//    fun `createNewEntity   ID increment`() {
+//        // Verify that the returned ID from createNewEntity is one more
+//        //than the previous one
+//        for (i in 3..10) {
+//            val entityId = EntityManager.createNewEntity()
+//            assert(entityId == i) { "First entity ID should be ${i}, was $entityId" }
+//        }
+//    }
 
 
-    @Test
-    fun `createNewEntity   multiple calls`() {
-        // Create multiple new entities (e.g., 100) and verify 
-        // that each entity's ID is unique and properly incremented.
-        for (i in 11..100) {
-            val entityId = EntityManager.createNewEntity()
-            assert(entityId == i) { "First entity ID should be ${i}, was $entityId" }
-        }
-    }
+//    @Test
+
+//    fun `createNewEntity   multiple calls`() {
+//        // Create multiple new entities (e.g., 100) and verify
+//        // that each entity's ID is unique and properly incremented.
+//        for (i in 11..100) {
+//            val entityId = EntityManager.createNewEntity()
+//            assert(entityId == i) { "First entity ID should be ${i}, was $entityId" }
+//        }
+//    }
 
 
 }
