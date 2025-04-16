b8a56362375ab3954d802f1ebfe0e0f2a6e3c2dd
Qmppu842 <6827447+Qmppu842@users.noreply.github.com>
2025-04-16 17:54:21 +0300

    feat: test that player has discard deck

---
+++ app/src/test/java/com/sq/thed_ck_licker/ecs/systems/characterSystems/PlayerSystemTest.kt
@@ -0,0 +1,24 @@
+package com.sq.thed_ck_licker.ecs.systems.characterSystems
+
+import com.sq.thed_ck_licker.ecs.EntityManager
+import com.sq.thed_ck_licker.ecs.components.DiscardDeckComponent
+import com.sq.thed_ck_licker.ecs.get
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.fail
+import org.junit.jupiter.api.Test
+
+class PlayerSystemTest {
+    @Test
+    fun `player has discard deck`() {
+        val playerId = EntityManager.getPlayerID()
+        PlayerSystem.instance.initPlayer()
+        var discardDeck: DiscardDeckComponent? = null
+        try {
+            discardDeck = playerId get DiscardDeckComponent::class
+        } catch (e: Exception) {
+            fail("Player has no discard deck")
+        }
+        assertNotNull(discardDeck)
+    }
+
+}
