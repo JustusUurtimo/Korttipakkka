02a31c097ffc2fca1c9062ec41b026a9d2098c6d
Qmppu842 <6827447+Qmppu842@users.noreply.github.com>
2025-04-24 16:28:16 +0300

    TDD TCR to the Extreme

--- app/src/main/java/com/sq/thed_ck_licker/ecs/systems/helperSystems/MultiplierSystem.kt
+++ app/src/main/java/com/sq/thed_ck_licker/ecs/systems/helperSystems/MultiplierSystem.kt
@@ -5,6 +5,7 @@ import com.sq.thed_ck_licker.ecs.EntityId
 import com.sq.thed_ck_licker.ecs.components.MultiplierComponent
 import com.sq.thed_ck_licker.ecs.difference
 import com.sq.thed_ck_licker.ecs.get
+import kotlin.reflect.full.declaredMembers
 
 fun multiplyEntityValues(oldEntityId: EntityId, targetEntityId: EntityId) {
     if (oldEntityId == targetEntityId) return
@@ -17,8 +18,10 @@ fun multiplyEntityValues(oldEntityId: EntityId, targetEntityId: EntityId) {
     val multiplier = (targetEntityId get MultiplierComponent::class).multiplier - 1
 
     for (component in diffComponents) {
-        val declaredThings = component::class.members
+        val declaredThings = component::class.declaredMembers
         println("Declared Things: $declaredThings")
+        for (declaredThing in declaredThings) {
+        }
     }
 
 }
