08b6b88cfba4ca1097f1927076fa68644ce33cd3
Qmppu842 <6827447+Qmppu842@users.noreply.github.com>
2025-05-21 12:01:35 +0300

    Proof of concept: Periodic damage effect
    A checkbox for "hurryness" has been added to the settings screen.
    When checked, the player will take 1 damage every second.

--- app/src/main/java/com/sq/thed_ck_licker/ecs/systems/viewSystems/navigationViews/screens/SettingsScreen.kt
+++ app/src/main/java/com/sq/thed_ck_licker/ecs/systems/viewSystems/navigationViews/screens/SettingsScreen.kt
@@ -1,8 +1,15 @@
 package com.sq.thed_ck_licker.ecs.systems.viewSystems.navigationViews.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 @Composable
@@ -10,6 +17,31 @@ fun SettingsScreen(modifier: Modifier) {
     Column (modifier = modifier) {
         Text("Settings")
         Text("Not implemented yet")
+        HurryCheckbox()
     }
 
 }
+
+val checked = mutableStateOf(true)
+
+@Composable
+fun HurryCheckbox() {
+    var checked2 by remember { checked }
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(
+            "Hurryness"
+        )
+        Checkbox(
+            checked = checked2,
+            onCheckedChange = { checked2 = it }
+        )
+    }
+
+    Text(
+        if (checked2) "Now you must hurry mode" else "Relaxed mode"
+    )
+}
+
