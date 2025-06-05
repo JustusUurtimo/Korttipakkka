0a14c97bddc0c4858fe5252a5b87b25153e06b67
Qmppu842 <6827447+Qmppu842@users.noreply.github.com>
2025-06-05 20:59:04 +0300

    Added a function to shuffle effects around to different triggers

--- app/src/main/java/com/sq/thed_ck_licker/ecs/components/EffectComponent.kt
+++ app/src/main/java/com/sq/thed_ck_licker/ecs/components/EffectComponent.kt
@@ -1,5 +1,7 @@
 package com.sq.thed_ck_licker.ecs.components
 
+import com.sq.thed_ck_licker.helpers.MyRandom.random
+
 /**
  * Used to make more complex activations
  * The functions take the target entity id as a parameter.
@@ -40,6 +42,26 @@ data class EffectComponent(
             onSpawn = onSpawn,
             onTurnStart = onTurnStart,
             onDeactivate = onDeactivate
+        )
+    }
+
+    fun shuffleToNew(): EffectComponent {
+        val ekat = mutableListOf<(Int) -> Unit>()
+        ekat.add(onDeath)
+        ekat.add(onSpawn)
+        ekat.add(onTurnStart)
+
+        // TODO: This is way more fun when these two can be combined into one.
+        val tokat = mutableListOf<(Int, Int) -> Unit>()
+        tokat.add(onPlay)
+        tokat.add(onDeactivate)
+
+        return EffectComponent(
+            onDeath = ekat.removeAt(random.nextInt(ekat.size)),
+            onSpawn = ekat.removeAt(random.nextInt(ekat.size)),
+            onTurnStart = ekat.removeAt(random.nextInt(ekat.size)),
+            onPlay = tokat.removeAt(random.nextInt(tokat.size)),
+            onDeactivate = tokat.removeAt(random.nextInt(tokat.size))
         )
     }
 }
