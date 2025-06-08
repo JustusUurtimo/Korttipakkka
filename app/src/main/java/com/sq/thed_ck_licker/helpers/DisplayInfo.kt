76697f17b237a57efbfd788854ab475703c9ae6c
Qmppu842 <6827447+Qmppu842@users.noreply.github.com>
2025-06-08 10:07:37 +0300

    Create DisplayInfo to display messages to the user

--- app/src/main/java/com/sq/thed_ck_licker/helpers/DisplayInfo.kt
+++ app/src/main/java/com/sq/thed_ck_licker/helpers/DisplayInfo.kt
@@ -1,3 +1,7 @@
 package com.sq.thed_ck_licker.helpers
 
+
+/**
+ * This can be used when you want to display something to user but there is no ui for it yet.
+ */
 fun displayInfo(text: String) = println(text)
