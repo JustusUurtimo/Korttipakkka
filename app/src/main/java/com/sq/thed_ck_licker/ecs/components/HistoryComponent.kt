540712b63068a61f14112d9350aece61479e1c28
Qmppu842 <6827447+Qmppu842@users.noreply.github.com>
2025-06-12 16:33:45 +0300

    feat: Introduce TargetComponent
    This commit introduces a new component, `TargetComponent`, which allows an entity to specify another entity as its target.

--- app/src/main/java/com/sq/thed_ck_licker/ecs/components/HistoryComponent.kt
+++ app/src/main/java/com/sq/thed_ck_licker/ecs/components/HistoryComponent.kt
@@ -2,4 +2,7 @@ package com.sq.thed_ck_licker.ecs.components
 
 import com.sq.thed_ck_licker.ecs.managers.EntityId
 
+/**
+ * @param history is reference to some entity, hopefully the same in past but no guarantees
+ */
 data class HistoryComponent(val history: EntityId)
