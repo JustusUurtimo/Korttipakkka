08e6e81f6d322479ed693918000612ccad3538fd
Qmppu842 <6827447+Qmppu842@users.noreply.github.com>
2025-06-26 11:31:10 +0300

    Refactor: Simplify TriggeredEffectsComponent instantiation
    The `TriggeredEffectsComponent` can now be instantiated with a simpler constructor, making card creation more concise.
    
    This is really nice again.

--- app/src/main/java/com/sq/thed_ck_licker/ecs/components/effectthing/TriggeredEffectsComponent.kt
+++ app/src/main/java/com/sq/thed_ck_licker/ecs/components/effectthing/TriggeredEffectsComponent.kt
@@ -2,6 +2,22 @@ package com.sq.thed_ck_licker.ecs.components.effectthing
 
 import com.sq.thed_ck_licker.ecs.components.Component
 
+/**
+ * These two are functionally identical:
+ * cardId add TriggeredEffectsComponent(mutableMapOf(Trigger.OnPlay to mutableListOf(Effect.TakeDamage(damageAmount))))
+ * and
+ * cardId add TriggeredEffectsComponent(Trigger.OnPlay, Effect.TakeDamage(damageAmount))
+ *
+ * You can also go wild like:
+ * cardId add TriggeredEffectsComponent(Trigger.OnPlay, Effect.TakeDamage(damageAmount), Effect.GainHealth(healAmount))
+ */
 data class TriggeredEffectsComponent(
-    val effectsByTrigger: MutableMap<Trigger, MutableList<Effect>> = mutableMapOf()
-) : Component
+    val effectsByTrigger: Map<Trigger, List<Effect>> = mapOf()
+) : Component {
+    /**
+     * This constructor is perfect when you want to have one or more effects that are tied to one trigger
+     */
+    constructor(trigger: Trigger = Trigger.OnPlay, vararg effects: Effect) : this(
+        mutableMapOf(trigger to effects.toList())
+    )
+}
