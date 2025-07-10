4b9a4fa9b270f8628163fccd12d641690a9b0186
Qmppu842 <6827447+Qmppu842@users.noreply.github.com>
2025-07-10 16:56:15 +0300

    Added couple of effects, mainly conditionals. And then made the time bound card use some of them. I admit the new style is really verbose, but I still think it is really neat overall.
    File count grew so fast...
    
    It is not *that* big of thing.
    
    I did not test them all but they all should "just work" they are quite simple.
    But it will be fun to track down if any bugs arise.

--- app/src/main/java/com/sq/thed_ck_licker/ecs/components/effectthing/conditionals/FlipTargets.kt
+++ app/src/main/java/com/sq/thed_ck_licker/ecs/components/effectthing/conditionals/FlipTargets.kt
@@ -3,17 +3,10 @@ package com.sq.thed_ck_licker.ecs.components.effectthing.conditionals
 import com.sq.thed_ck_licker.ecs.components.effectthing.Effect
 import com.sq.thed_ck_licker.ecs.components.effectthing.EffectContext
 
-data class OnActivations(
-    var current: Int = 0,
-    val threshold: Int,
-    val effect: Effect
-) : ConditionalEffect() {
+data class FlipTargets(val effect: Effect) : ConditionalEffect() {
     override fun execute(context: EffectContext): Float {
-        this.current++
-        if (this.current > threshold) {
-            this.current -= threshold
-            return effect.executeCall(context)
-        }
+        val contextHack = context.copy(target = context.source, source = context.target)
+        effect.executeCall(contextHack)
         return 0f
     }
 }
